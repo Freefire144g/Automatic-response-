26bce3e64d826e24ec01ee75fd68bfd61a70b183
Freefire144g <123093489+Freefire144g@users.noreply.github.com>
2025-02-04 12:06:55 +0300

    Click here
    Signed-off-by: Freefire144g <123093489+Freefire144g@users.noreply.github.com>

---
+++ رد.py
@@ -0,0 +1,24 @@
+from telethon import TelegramClient, events
+import time
+
+api_id = YOUR_API_ID
+api_hash = 'YOUR_API_HASH'
+client = TelegramClient('auto_reply_bot', api_id, api_hash)
+
+replied_users = set()
+
+@client.on(events.NewMessage)
+async def handle_message(event):
+    try:
+        if event.is_private and event.sender_id not in replied_users:
+            await event.respond("اهلا وسهلا عزيزي سجاد غير متوفر الان وانا الرد التلقائي 👾🤖")
+            replied_users.add(event.sender_id)
+            print(f"تم الرد على هذا الشخص برد تلقائي : {event.sender_id}")
+            time.sleep(2)
+
+    except Exception as e:
+        print(f"خطأ: {str(e)}")
+
+print("اشتغلت...")
+client.start()
+client.run_until_disconnected()
